2d6ee0bfb52f95f58c1f7d6dc750d0c796cf858f
Faza <ahmadfaza200@gmail.com>
2025-08-28 15:08:20 +0700

    tugas pertemuan 2

---
+++ acara2/tugas.php
@@ -0,0 +1,27 @@
+<?php
+$a = "10";
+$b = "20";
+
+$hasil = $a + $b; // PHP otomatis ubah string angka jadi integer
+echo $hasil; // Output: 30
+
+echo "<br/>";
+
+$x = 10;
+$y = 3;
+
+echo $x / $y; // Output: 3.333...
+echo "<br>";
+echo $x % $y; // Output: 1
+
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+
+$tugas1 = "90";
+$tugas2 = "80";
+
+echo  $tugas1 . $tugas2; 
+
+?>
+
